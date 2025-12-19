a66427c41f485b4680f74429b745dffb0e069cd6
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-12-19 18:54:00 +0100

    Create MemoryPool header with basic setup

---
+++ cpp-memory-pool-cache/MemoryPool.cpp
@@ -0,0 +1,8 @@
+//
+//  MemoryPool.cpp
+//  cpp-memory-pool-cache
+//
+//  Created by MEDLAO on 19/12/2025.
+//
+
+#include "MemoryPool.hpp"
---
+++ cpp-memory-pool-cache/MemoryPool.hpp
@@ -0,0 +1,15 @@
+//
+//  MemoryPool.hpp
+//  cpp-memory-pool-cache
+//
+//  Created by MEDLAO on 19/12/2025.
+//
+
+#ifndef MemoryPool_hpp
+#define MemoryPool_hpp
+
+#include <stdio.h>
+#pragma once
+#include <cstddef>  // for size_t
+
+#endif /* MemoryPool_hpp */
