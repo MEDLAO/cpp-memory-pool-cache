87ffb919253cc884f405cd8a8a8c0b211103af46
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-12-20 18:53:57 +0100

    Add MemoryPool class with constructor declaration

--- cpp-memory-pool-cache/MemoryPool.hpp
+++ cpp-memory-pool-cache/MemoryPool.hpp
@@ -12,4 +12,13 @@
 #pragma once
 #include <cstddef>  // for size_t
 
+class MemoryPool {
+public:
+    // Constructor: creates a pool with fixed-size blocks
+    MemoryPool(std::size_t blockSize, std::size_t blockCount);
+
+private:
+    unsigned char* buffer; // points to raw memory owned by the pool
+};
+
 #endif /* MemoryPool_hpp */
